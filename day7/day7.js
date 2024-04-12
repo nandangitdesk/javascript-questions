618c9a27b5a0811c6db3095a77b2c3c5c65518ab
Nandan Patil <150545490+nandangitdesk@users.noreply.github.com>
2024-04-12 10:20:59 +0530

    Add files via upload

---
+++ day7/day7.js
@@ -0,0 +1,8 @@
+const findMax = (arr)=>{
+   console.log(...arr)
+   return Math.max("max numbers",...arr)
+
+}
+console.log(findMax([1,4,5,7,9]))
+console.log(findMax([-1,-4,-5,-7,-9]))
+console.log(findMax([3]))
