4e80867c324e4f0421de1fea698594af9270d47a
Nandan Patil <150545490+nandangitdesk@users.noreply.github.com>
2024-04-30 11:31:12 +0530

    Add files via upload

---
+++ day25/day25.js
@@ -0,0 +1,12 @@
+const factorial = (num) => {
+    if (num === 1) {
+      return 1;
+    } else {
+      return  num * factorial(num - 1);
+    }
+  };
+  
+  console.log(factorial(5));
+  console.log(factorial(7));
+  console.log(factorial(10));
+  
